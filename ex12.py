10fa22da4d321b9fd0fef0ddbbfd2ac14599b132
Theintnandarzaw <Nandarzaw68@gmail.com>
2018-06-11 11:47:58 +0630

    another pythin ex12

--- ex12.py
+++ ex12.py
@@ -1,6 +1,6 @@
 
-age=raw_input("How old ara you?")
-height=raw_input("How tall are you?")
-weight=raw_input("How much do you weigth?")
+age=input("How old ara you?")
+height=input("How tall are you?")
+weight=input("How much do you weigth?")
 
 print("So ,you're %r old , %r tall and %r heavy ." %(age,height,weight))
