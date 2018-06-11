6db4c87cb0ddbceb81e86c0bc8b1925d31980b1c
Theintnandarzaw <Nandarzaw68@gmail.com>
2018-06-11 17:10:49 +0630

    Python ex16

--- ex11.py
+++
@@ -1,9 +0,0 @@
-print("How old are you?",end=' ')
-age=input()
-
-print("how tall are you?",end=' ')
-height=input()
-print("how much do you weight?",end=' ')
-weight=input()
-print("So ,you're %r old ,%r tall and %r heavy." %(age,height,weight))
-
